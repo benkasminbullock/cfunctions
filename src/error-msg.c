547b006c7266fd9150bb543bc580f9845ac61fca
Ben Bullock <benkasminbullock@gmail.com>
2017-04-14 11:01:07 +0900

    Remove header and tidy error-msg.c

--- src/error-msg.c
+++ src/error-msg.c
@@ -2,13 +2,9 @@
 
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "error-msg.h"
-
-#ifdef HEADER
-#define HERE __FILE__, __LINE__
-#endif /* HEADER */
-#include <stdlib.h>
 
 /* The name of the program running and of the source file the program
    is reading from.  The default values are just for debugging. */
@@ -17,120 +13,123 @@ const char * program_name = "PROGRAM NAME UNDEFINED";
 const char * source_name = "SOURCE NAME UNDEFINED";
 int * source_line = NULL;
 
-
-#define DO_PRINT(file) \
-  va_start (a, format); \
-  vfprintf (file, format, a); \
-  fprintf (file, ".\n"); \
-  va_end (a)
+#define DO_PRINT(file)				\
+    va_start (a, format);			\
+    vfprintf (file, format, a);			\
+    fprintf (file, ".\n");			\
+    va_end (a)
 
 
 /* Print a warning message with a line number.  */
 
-void PRINT_FORMAT(2,3)
-lwarning (unsigned line_number, const char * format, ...)
+void PRINT_FORMAT (2, 3)
+    lwarning (unsigned line_number, const char * format, ...)
 {
-  va_list a;
+    va_list a;
 
-  fprintf (stderr, "%s:%u: warning: ", source_name, line_number);
+    fprintf (stderr, "%s:%u: warning: ", source_name, line_number);
 
-  DO_PRINT (stderr);
+    DO_PRINT (stderr);
 }
 
 /* Print an error message for a specified line and then exit */
 
-NO_RETURN PRINT_FORMAT(1,2)
+NO_RETURN PRINT_FORMAT (1, 2)
 line_error (const char * format, ... )
 {
-  va_list a;
+    va_list a;
 
-  fprintf (stderr, "%s:%u: error: ", source_name, * source_line);
+    fprintf (stderr, "%s:%u: error: ", source_name, * source_line);
 
-  DO_PRINT (stderr);
+    DO_PRINT (stderr);
 
-  exit (EXIT_FAILURE);
+    // exit ok
+    exit (EXIT_FAILURE);
 }
 
 /* Print a warning message with a line number.  */
 
-void PRINT_FORMAT(1,2)
-line_warning (const char * format, ... )
+void PRINT_FORMAT (1, 2)
+    line_warning (const char * format, ... )
 {
-  va_list a;
+    va_list a;
 
-  fprintf (stderr, "%s:%u: warning: ", source_name, * source_line);
+    fprintf (stderr, "%s:%u: warning: ", source_name, * source_line);
 
-  DO_PRINT (stderr);
+    DO_PRINT (stderr);
 }
 
 /* Print a warning message with a line number.  */
 
-void PRINT_FORMAT(1,2)
+void PRINT_FORMAT (1, 2)
 line_info (const char * format, ... )
 {
-  va_list a;
+    va_list a;
 
-  fprintf (stdout, "%s:%u: ", source_name, * source_line);
+    fprintf (stdout, "%s:%u: ", source_name, * source_line);
 
-  DO_PRINT (stdout);
+    DO_PRINT (stdout);
 }
 
 /* Print an error message for a specified line and then exit */
 
-NO_RETURN PRINT_FORMAT(2,3)
+NO_RETURN PRINT_FORMAT (2, 3)
 lerror ( unsigned line_number, const char * format, ... )
 {
-  va_list a;
+    va_list a;
 
-  fprintf ( stderr, "%s:%u: error: ", source_name, line_number );
+    fprintf ( stderr, "%s:%u: error: ", source_name, line_number );
 
-  DO_PRINT (stderr);
+    DO_PRINT (stderr);
 
-  exit (EXIT_FAILURE);
+    // exit ok
+    exit (EXIT_FAILURE);
 }
 
 /* Print the specified error message and exit. */
 
-NO_RETURN PRINT_FORMAT(1,2)
+NO_RETURN PRINT_FORMAT (1, 2)
 error (const char * format, ... )
 {
-  va_list a;
+    va_list a;
 
-  fprintf ( stderr, "%s: error: ", program_name );
+    fprintf ( stderr, "%s: error: ", program_name );
 
-  DO_PRINT (stderr);
+    DO_PRINT (stderr);
 
-  exit (EXIT_FAILURE);
+    // exit ok
+    exit (EXIT_FAILURE);
 }
 
 /* Print a warning message. */
 
-void PRINT_FORMAT(1,2)
+void PRINT_FORMAT (1, 2)
 warning (const char * format, ... )
 {
-  va_list a;
+    va_list a;
 
-  fprintf ( stderr, "%s: warning: ", program_name );
+    fprintf ( stderr, "%s: warning: ", program_name );
   
-  DO_PRINT (stderr);
+    DO_PRINT (stderr);
 }
 
 /* Print an internal error message.  This is for the case that
    something that should never happen occurs, such as a `default:' in
    a switch statement that is used to catch errors. */
 
-NO_RETURN  PRINT_FORMAT(3,4)
+NO_RETURN  PRINT_FORMAT (3, 4)
 bug (const char * file, unsigned line, const char * format, ... )
 {
-  va_list a;
+    va_list a;
 
-  fprintf ( stderr, "%s:%s:%u: internal error: ", 
-            program_name, file, line );
-  fprintf ( stderr, "%s:%s:%u: internal error: ", 
-            program_name, file, line );
+    fprintf ( stderr, "%s:%s:%u: internal error: ", 
+	      program_name, file, line );
+    fprintf ( stderr, "%s:%s:%u: internal error: ", 
+	      program_name, file, line );
 
-  DO_PRINT (stderr);
+    DO_PRINT (stderr);
 
-  exit (EXIT_FAILURE);
+    // exit ok
+    exit (EXIT_FAILURE);
 }
 
