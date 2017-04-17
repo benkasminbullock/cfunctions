00f56949a0c0a0c283e8f56ed4a30db5f3df555a
Ben Bullock <benkasminbullock@gmail.com>
2017-04-17 23:20:44 +0900

    Remove unused defines.h and defines.h.in

--- defines.h
+++
@@ -1,6 +0,0 @@
-/* src/defines.h.  Generated from defines.h.in by configure.  */
-#define HAVE_SYS_WAIT_H 1
-#define HAVE_UNISTD_H 1
-#define HAVE_GETOPT_LONG 1
-#define HAVE_STRDUP 1
-#define HAVE_STRERROR 1
