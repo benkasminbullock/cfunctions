57a3a83b7e3112ce9795c3d9e452af89dd644aa7
Ben Bullock <benkasminbullock@gmail.com>
2017-04-20 11:53:15 +0900

    Test exit status in test.pl, remove options testing
    Clean up Perl code and use IPC::Run3 to run stuff.

--- tests/test.pl
+++ tests/test.pl
@@ -13,11 +13,12 @@ use C::Utility 'c_to_h_name';
 
 chdir $Bin or die $!;
 my $cfunctions = "$Bin/../cfunctions";
+# Make sure that the executable exists.
 if (! -x $cfunctions) {
     die "No $cfunctions; need to rebuild?";
 }
-my $cc         = "cc";
-my $GNU_c      = 1;
+# C compiler command.
+my $cc = "cc";
 main ();
 done_testing ();
 rm_h_files ();
@@ -25,51 +26,27 @@ exit;
 
 sub main
 {
-
-    # Make sure that the executable Cfunctions does exist.
-
-    if ( ! -e $cfunctions || ! -x $cfunctions ) {
-	print STDERR 
-        "$0: '$cfunctions' cannot be found or is not executable.\n";
-	exit ( 1 );
-    }
-
     rm_h_files();
-
-    opendir(THISDIR, ".");
-
-    my @bad_files = grep ( /^bad-.*\.c$/, readdir THISDIR);
-
-    closedir THISDIR;
-
+    my @bad_files = grepdir ( qr/^bad-.*\.c$/);
     foreach my $bad_file (@bad_files) {
-	err_test ( $bad_file, "error" );
+	err_test ($bad_file, "error");
     }
-
     # Try running Cfunctions on each `warn-' file and see if it generates
     # a warning message.
 
-    opendir(THISDIR, ".");
-
-    my @warn_files = grep ( /^warn-.*\.c$/, readdir THISDIR);
-
+    my @warn_files = grepdir ( qr/^warn-.*\.c$/);
     foreach my $warn_file (@warn_files) {
-	err_test ( $warn_file, "warning" );
+	err_test ($warn_file, "warning");
     }
-
-    opendir(THISDIR, ".");
-
-    my @ok_files = grep ( /^ok-.*\.c$/, readdir THISDIR);
-
+    my @ok_files = grepdir (qr/^ok-.*\.c$/);
     foreach my $ok_file (@ok_files) {
-	try_ok ( $ok_file );
+	try_ok ($ok_file);
     }
+
 
     rm_h_files();
-
-    if (-f 'a.out') {
-	unlink ( "a.out" ) || die;
-    }
+    test_not_c_warning ();
+    rm_h_files();
     test_backup ();
     rm_h_files ();
     test_verbatim ();
@@ -82,7 +59,33 @@ sub main
     rm_h_files ();
 }
 
-# Remove the backups
+sub test_not_c_warning
+{
+    my $filename = "$Bin/not-c-file-name.funkyjunk";
+    rm_h_files ();
+    write_garbage ($filename);
+    run3 ("cfunctions $filename", undef, \my $output, \my $errors);
+    like ($errors, qr/does not look like a C file/,
+	  "warning for non-c-file name $filename");
+    ok (! $?, "no error status for bad file name");
+    if (-f $filename) {
+	unlink $filename or die $!;
+    }
+    my $hfile = $filename;
+    $hfile =~ s/.$/h/;
+    ok (-f $hfile, "made the header file anyway");
+    if (-f $hfile) {
+	unlink $hfile or die $!;
+    }
+    rm_h_files ();
+}
+
+# This tests the backup facility. The backup facility has two
+# functions, first of all it backs up old .h files into .h.~1~ etc,
+# and secondly it un-backs-up the old .h file if the new file is the
+# same as the old one (see unbackup in wt.c) so that unnecessary
+# builds are not performed for unchanged files. This tests both of
+# these.
 
 sub test_backup
 {
@@ -136,84 +139,63 @@ sub write_garbage
     close $out or die $!;
 }
 
+sub grepdir
+{
+    my ($re) = @_;
+    if (ref $re ne 'Regexp') {
+	die "Not a regex";
+    }
+    opendir (THISDIR, $Bin) or die $!;
+    my @files = grep /$re/, readdir THISDIR;
+    closedir THISDIR or die $!;
+    return @files;
+}
+
 # Remove header files, backup header files and other generated files
 # from the current directory.
 
 sub rm_h_files
 {
-    opendir(THISDIR, ".") or die $!;
-    my @h_files = grep ( /\.h(?:\.~[0-9]+~)?$/, readdir THISDIR);
-
-    closedir THISDIR or die $!;
-
+    my @h_files = grepdir (qr/\.h(?:\.~[0-9]+~)?$/);
     for my $h_file (@h_files) {
         unlink $h_file or die $!;
     }
+    if (-f 'a.out') {
+	unlink ( "a.out" ) or die $!;
+    }
 }
+
+# Test production of error and warning messages.
 
 sub err_test
 {
-    my $c_file = $_[0];
-    my $message_type = $_[1];
-    my $options = "";
+    my ($c_file, $message_type) = @_;
 
-    my $test = "FAILED";
-
-# Get the error message from the C file.  Every `bad-' and `warn-'
-# file should have exactly one error message in it, which is a
-# substring of the error message it is supposed to trigger in
-# Cfunctions.  This is marked by `$message_type'.
-
-    open ( my $C_FILE, "<", $c_file ) or die;
-
-    my $msg = "";
-
-    while ( <$C_FILE> )
-    {
-        if ( /\/\/\s*options:\s*(.*)/ )
-        {
-            $options = $1;
-        }
-        if ( /\/\/\s*$message_type:\s*(.*)/ )
-        {
-            if ( $msg )
-            {
-                print "bad test: too many `$message_type' statements.\n";
-                close $C_FILE;
-                next;
-            }
-            $msg = $1;
-        }
+    my $text = read_text ($c_file);
+    my $msg = '';
+    # Get the error message from the C file.  Every `bad-' and `warn-'
+    # file should have exactly one error message in it, which is a
+    # substring of the error message it is supposed to trigger in
+    # Cfunctions.  This is marked by `$message_type'.
+    if ($text =~ m!//\s*$message_type:\s*(.*)!) {
+	$msg = $1;
+    }
+    my $runcommand = "$cfunctions $c_file"; 
+    if (! $msg) {
+        die "bad test $c_file: no `$message_type' statements.\n"; 
     }
 
-    my $runcommand = "$cfunctions $options $c_file"; 
-#    note "$runcommand: ";
+    # Try running Cfunctions on the C file and see if it generates an
+    # error message containing the right substrings.
 
-    if ( ! $msg ) {
-        note "bad test: no `$message_type' statements.\n"; 
+    run3 ($runcommand, undef, \my $output, \my $errors);
+    if ($message_type eq 'error') {
+	ok ($?, "Got error status from $c_file");
     }
-
-    close $C_FILE or die $!;
-
-# Try running Cfunctions on the C file and see if it generates an
-# error message containing the right substrings.
-
-    open ( my $TEST_OUT, "$runcommand 2>&1 |" ) or die $!;
-
-    while ( <$TEST_OUT> )
-    {
-        if ( /$message_type/ && /$msg/ )
-        {
-            $test = "PASSED";
-        }
+    else {
+	ok (! $?, "No error status from $c_file");
     }
-    # Because cfunctions exited with an error status, "close" will
-    # return a non-zero value.
-#    close $TEST_OUT or die $!;
-
-    unlike ( $test, qr/FAILED/, "cfunctions made message $message_type processing $c_file" );
-
-    return;
+    like ($errors, qr/\Q$message_type\E.*\Q$msg\E/);
 }
 
 # Run Cfunctions on the PASSED files.  If Cfunctions gives an error
@@ -223,35 +205,14 @@ sub try_ok
 {
     my $c_file = $_[0];
     my $failed = 0;
-    my $options = "";
     my $error_msg = "";
     my $test = "PASSED";
 
-# Get the options from the C file, if there are any.
+    # Try running Cfunctions on the C file and see if it generates an
+    # error message containing the right substrings.
 
-    open ( C_FILE, $c_file ) || die;
-
-    while ( <C_FILE> )
-    {
-        if ( /\/\/\s*options:\s*(.*)/ )
-        {
-            $options = $1;
-        }
-    }
-
-    close C_FILE;
-
-#    note "$cfunctions $options $c_file: "; 
-
-# Try running Cfunctions on the C file and see if it generates an
-# error message containing the right substrings.
-
-    open ( TEST_OUT, "$cfunctions $c_file 2>&1 |" );
-
-    while ( <TEST_OUT> ) {
-	$error_msg .= $_;
-    }
-    close TEST_OUT;
+    run3 ("$cfunctions $c_file", undef, \my $output, \$error_msg);
+    ok (! $?, "No error status from $c_file");
     ok (! $error_msg, "no errors with $cfunctions $c_file");
     if ($error_msg) {
 	diag ("Cfunctions produced errors as follows: $error_msg");
@@ -261,49 +222,24 @@ sub try_ok
 
     my $link_file = $c_file;
     $link_file =~ s/^ok-/link-/;
-    my $cc_op = "";
-    my $gcc_op;
-    if ( ! $GNU_c )
-    {
-        $gcc_op = "";
-    }
-    else
-    {
-        $gcc_op = "-Wall ";
-    }
-    open ( LINK_FILE, $link_file ) || die "no link file for $c_file";
+    my $cc_op = " -Wall";
+    open my $lf, "<", $link_file or die "no link file for $c_file";
 
-    while (<LINK_FILE>) {
+    while (<$lf>) {
         if ( /\/\/\s*options:\s*(.*)/ ) {
-            $cc_op .= $1;
+            $cc_op .= " $1";
         }
-        if ( $GNU_c ) {
-            if ( /\/\/\s*gcc_opt:\s*(.*)/ ) {
-                $gcc_op .= $1;
-            }
-        }
+	if ( /\/\/\s*gcc_opt:\s*(.*)/ ) {
+	    $cc_op .= " $1";
+	}
     }
 
-    close LINK_FILE;
-
-#    note "$cc $cc_op $gcc_op $c_file $link_file: "; 
+    close $lf or die $!;
 
     my $cc_error_msg = "";
 
-    # The following `2>&1' directs `stderr' to Perl and `stdout' to
-    # `/dev/null'.
-
-    open ( CC_OUTPUT, '-|', 
-           "$cc $cc_op $gcc_op $c_file $link_file 2>&1 >/dev/null" );
-
-    while ( <CC_OUTPUT> ) {
-        if ( /.+/ ) {
-            $cc_error_msg .= $_;
-        }
-    }
-
-    close CC_OUTPUT;
-
+    run3 ("$cc $cc_op $c_file $link_file",
+	  undef, \my $cc_output, \$cc_error_msg);
     ok (! $cc_error_msg, "no errors compiling $c_file and $link_file");
     if ($cc_error_msg) {
 	diag ("Compiler error is '$cc_error_msg'");
--- tests/warn-not-c.c
+++
@@ -1,3 +0,0 @@
-// warning: does not look like a C file
-
-// options: funky_junk.notc
